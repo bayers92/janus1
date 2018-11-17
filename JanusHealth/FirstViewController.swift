8e46f44b95116fcc8cc9406ccc4874c0c3786580
bayers92 <brianc.ayers@gmail.com>
2018-11-17 14:06:05 -0500

    Initial Commit

---
+++ JanusHealth/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  JanusHealth
+//
+//  Created by bayers on 11/17/18.
+//  Copyright © 2018 bayers. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
